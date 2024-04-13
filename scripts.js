ab35b9dd276e7e3971e20f1ea7beed1424fcd689
Raghvendra <rannyshukla762@gmail.com>
2024-04-13 19:39:56 +0530

    update the search bar

--- scripts.js
+++ scripts.js
@@ -3,12 +3,10 @@ const searchBar = document.getElementById('search-bar');
 
   searchBar.addEventListener('input', function() {
     const searchText = searchBar.value.toLowerCase();
-
     for (let i = 0; i < productList.length; i++) {
       const product = productList[i];
       const productName = product.querySelector('h2').innerText.toLowerCase();
-
-      if (productName.includes(searchText)) {
+      if (productName.startsWith(searchText)) {
         product.style.display = 'block';
       } else {
         product.style.display = 'none';
