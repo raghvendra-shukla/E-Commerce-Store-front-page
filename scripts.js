ba3a455c468e8a3e2995fab56fb5741311f36302
Raghvendra <rannyshukla762@gmail.com>
2024-04-13 10:48:57 +0530

    2nc commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const searchBar = document.getElementById('search-bar');
+  const productList = document.getElementById('product-list').children;
+
+  searchBar.addEventListener('input', function() {
+    const searchText = searchBar.value.toLowerCase();
+
+    for (let i = 0; i < productList.length; i++) {
+      const product = productList[i];
+      const productName = product.querySelector('h2').innerText.toLowerCase();
+
+      if (productName.includes(searchText)) {
+        product.style.display = 'block';
+      } else {
+        product.style.display = 'none';
+      }
+    }
+  });
